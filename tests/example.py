21057c883ad59950c2279c91c9714a688862304d
Aiden Tabrizi <aidentabrizi@icloud.com>
2023-07-21 14:09:26 -0700

    Update comments on test

--- tests/example.py
+++ tests/example.py
@@ -9,4 +9,4 @@ var1.start()
 var2.start()
 
 # At this point, the threads are running and updating the variables in the background.
-# You can now use the variables in your new program as needed, and they will be updated automatically.
+# You can now use the variables in your new program as needed, and they will be updated when the update command is ran.
