c0f9079bc0875d45de03c59379fc17ca62a72ef6
soheilzi <soheil.zibakhsh@gmail.com>
2023-09-12 14:19:30 -0700

    version 0.0.4 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="LiveTune",
-    version="0.0.3",
+    version="0.0.4",
     author="Soheil Zibakhsh Shabgahi, Aiden Tabrizi",
     author_email="szibakhshshabgahi@ucsd.edu, atabrizi@ucsd.edu",
     description="Python package for ML developers and researchers to change certain variables while their code is executing to make the task of training a ML project easier. This package will allow you to tune some parameters while your code is live from outside of the program.",
